f551e9e090a2834fe2265a8eaca11f434a2d11df
Franz Flores <andresfloresgallardo@hotmail.com>
2019-07-03 18:19:41 +1400

    Metodos de login y logout

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,17 +1,60 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { HttpClient } from  '@angular/common/http';
 import { tap } from  'rxjs/operators';
-import { Observable, BehaviorSubject } from  'rxjs';
-
 import { Storage } from  '@ionic/storage';
+import { EnvService } from './env.service';
+import { User } from '../models/user';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  isLoggedIn = false;
+  token:any;
 
-  AUTH_SERVER_ADDRESS:  string  =  'http://localhost:3000';
-  authSubject  =  new  BehaviorSubject(false);
+  constructor(
+    private  http:  HttpClient, 
+    private  storage:  Storage,
+    private env: EnvService
+    ) { }
 
-  constructor(private  httpClient:  HttpClient, private  storage:  Storage) { }
+    login(email: String,password:String) {
+      return this.http.post(`${this.env.API_URL}/login`, {email:email,password:password})
+      .pipe(
+        tap(token=>{
+          this.storage.set("token", token)
+          .then(
+          ()=>{
+            console.log("token guardado");
+          },
+          error => console.error("Error al guardar",error)
+          );
+          this.token= token;
+          this.isLoggedIn = true;
+          return token;
+        })
+      );
+    }
+
+    logout() {
+      const headers = new HttpHeaders({
+        'Authorization': this.token["token_type"]+" "+this.token["access_token"]
+      });
+      return this.http.get(this.env.API_URL + '/logout', { headers: headers })
+      .pipe(
+        tap(data => {
+          this.storage.remove("token")
+          .then(
+            ()=>{
+              console.log("token eliminado");
+            },
+            error => console.error("Error al guardar",error)
+            );
+          this.isLoggedIn = false;
+          delete this.token;
+          return data;
+        })
+      )
+    }
 }
