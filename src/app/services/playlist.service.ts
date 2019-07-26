a4b1f358f1f01fbe902ed662a617923f79723b7b
Franz Flores <andresfloresgallardo@hotmail.com>
2019-07-26 14:00:33 +1400

    Servicio Plaulist

--- src/app/services/playlist.service.ts
+++ src/app/services/playlist.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Playlist } from '../models/playlist';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlaylistService {
 
-  constructor() { }
+  AUTH_SERVER_ADDRESS: string = 'http://localhost:3000';
+  playlists: Playlist[];
+
+  constructor(private httpClient: HttpClient) { }
+
+  getPlaylists(){
+    return this.httpClient.get(`${this.AUTH_SERVER_ADDRESS}/playlist/playlistAdmin`);
+  }
+
+  getPlaylist(external){
+    return this.httpClient.get(`${this.AUTH_SERVER_ADDRESS}/playlist/`+ external);
+  }
+
 }
