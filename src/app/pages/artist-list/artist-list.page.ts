a144c03f50fc018c1f7ed7b8062cf3031b48cea4
luis <villaltaluis1997@gmail.com>
2019-07-26 09:10:40 -0500

    configuraciones pagina artista

--- src/app/pages/artist-list/artist-list.page.ts
+++ src/app/pages/artist-list/artist-list.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ArtistService } from 'src/app/services/artist.service';
+import { Artist} from 'src/app/models/artist';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-artist-list',
@@ -6,10 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./artist-list.page.scss'],
 })
 export class ArtistListPage implements OnInit {
+  artist:Artist;
 
-  constructor() { }
+  constructor(private artistService:ArtistService) { }
 
   ngOnInit() {
+    this.getArtists();
   }
 
+  getArtists(){
+    this.artistService.getArtists().subscribe(res=>{
+      this.artistService.artists=res as Artist[];
+    });
+  }
 }
