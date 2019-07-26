8dd339e7549ca90ae3385aa2d310051d31918592
luis <villaltaluis1997@gmail.com>
2019-07-26 08:57:46 -0500

    servicio artista

--- src/app/services/artist.service.ts
+++ src/app/services/artist.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Artist } from '../models/artist';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArtistService {
+  AUTH_SERVER_ADDRESS: string = 'http://localhost:3000';
+  artists: Artist[];
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getArtists() {
+    return this.httpClient.get(`${this.AUTH_SERVER_ADDRESS}/artist/artists`);
+  }
+
+  getArtist(external){
+    return this.httpClient.get(`${this.AUTH_SERVER_ADDRESS}/artist/`+ external);
+  }
 }
