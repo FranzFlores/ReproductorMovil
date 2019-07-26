d922ea08f9d3394309aafe69f682c14c6e70ff65
Alberto Ortega <betoandriu01@gmail.com>
2019-07-26 09:12:29 -0500

    configuración de página Album

--- src/app/pages/album-list/album-list.page.ts
+++ src/app/pages/album-list/album-list.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AlbumService } from 'src/app/services/album.service';
+import { Album } from 'src/app/models/album';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-album-list',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlbumListPage implements OnInit {
 
-  constructor() { }
+  album:Album;
+
+  constructor(private albumService:AlbumService) { }
 
   ngOnInit() {
+    this.getAlbums();
+  }
+
+  getAlbums(){
+    this.albumService.getAlbums()
+    .subscribe(res=>{
+      this.albumService.albums = res as Album[];
+    })
   }
 
 }
