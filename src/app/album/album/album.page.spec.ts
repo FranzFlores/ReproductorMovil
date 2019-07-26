05df9b372dd04cd0ee7c0a012f9ce2edd26c290d
luis <villaltaluis1997@gmail.com>
2019-07-26 08:49:50 -0500

    modelos artista
    modelo artista

---
+++ src/app/album/album/album.page.spec.ts
@@ -0,0 +1,24 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+
+
+describe('LoginPage', () => {
+  
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+     
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+  
+  });
+
+  it('should create', () => {
+    
+  });
+});
