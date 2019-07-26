382d97914273bcde64bd8e3c44a83d70784a9895
Alberto Ortega <betoandriu01@gmail.com>
2019-07-26 08:58:52 -0500

    servicio Album

--- src/app/services/album.service.ts
+++ src/app/services/album.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Album } from '../models/album';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AlbumService {
 
-  constructor() { }
+  AUTH_SERVER_ADDRESS: string = 'http://localhost:3000';
+  albums: Album[];
+  constructor(private httpClient: HttpClient) { }
+
+  getAlbums(){
+    return this.httpClient.get(`${this.AUTH_SERVER_ADDRESS}/album/albums`);
+  }
+
+  getAlbum(external){
+    return this.httpClient.get(`${this.AUTH_SERVER_ADDRESS}/album/`+ external);
+  }
 }
