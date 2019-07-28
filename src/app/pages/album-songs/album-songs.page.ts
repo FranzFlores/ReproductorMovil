c3aea11b749962c0ed214067c701f157584d9842
Franz Flores <andresfloresgallardo@hotmail.com>
2019-07-29 13:14:37 +1400

    Generacion de lista de canciones por Album

---
+++ src/app/pages/album-songs/album-songs.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-album-songs',
+  templateUrl: './album-songs.page.html',
+  styleUrls: ['./album-songs.page.scss'],
+})
+export class AlbumSongsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
