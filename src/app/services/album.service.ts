57afd2c52e62e757989f8d047c63c5549fa6ae66
Alberto Ortega <betoandriu01@gmail.com>
2019-07-29 10:21:06 -0500

    corección lista de canciones de álbum

--- src/app/services/album.service.ts
+++ src/app/services/album.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Album } from '../models/album';
+import {Song} from '../models/song';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,7 @@ export class AlbumService {
 
   AUTH_SERVER_ADDRESS: string = 'http://localhost:3000';
   albums: Album[];
+  songs: Song[];
   constructor(private httpClient: HttpClient) { }
 
   getAlbums(){
@@ -18,5 +20,9 @@ export class AlbumService {
 
   getAlbum(external){
     return this.httpClient.get(`${this.AUTH_SERVER_ADDRESS}/album/`+ external);
+  }
+
+  getAlbumSongs(external){
+    return this.httpClient.get(`${this.AUTH_SERVER_ADDRESS}/album//getSongs/`+ external);
   }
 }
