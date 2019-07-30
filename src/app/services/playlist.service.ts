d93433c1720e010acd74c870934d723b2a1a8f2c
Franz Flores <andresfloresgallardo@gmail.com>
2019-07-30 12:23:12 -0500

    Correcciones en las paginas de album y playlist

--- src/app/services/playlist.service.ts
+++ src/app/services/playlist.service.ts
@@ -1,26 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-<<<<<<< HEAD
-<<<<<<< develop
-<<<<<<< develop
- 
-import { Playlist } from '../models/playlist';
-import { Song } from '../models/song';
-<<<<<<< develop
-=======
 
-=======
- 
->>>>>>> Configuraciones de Pagina Playlist
-import { Playlist } from '../models/playlist';
->>>>>>> Servicio Plaulist
-=======
->>>>>>> Vista de Canciones de Playlist
-=======
  
 import { Playlist } from '../models/playlist';
 import { Song } from '../models/song';
->>>>>>> playlist
 
 @Injectable({
   providedIn: 'root'
@@ -29,18 +12,9 @@ export class PlaylistService {
 
   AUTH_SERVER_ADDRESS: string = 'http://localhost:3000';
   playlists: Playlist[];
-<<<<<<< HEAD
-<<<<<<< develop
-<<<<<<< develop
   songs: Song[];
-=======
->>>>>>> Servicio Plaulist
-=======
-  songs: Song[];
->>>>>>> Vista de Canciones de Playlist
-=======
-  songs: Song[];
->>>>>>> playlist
+
+ 
 
   constructor(private httpClient: HttpClient) { }
 
@@ -52,24 +26,8 @@ export class PlaylistService {
     return this.httpClient.get(`${this.AUTH_SERVER_ADDRESS}/playlist/`+ external);
   }
 
-<<<<<<< HEAD
-<<<<<<< develop
-<<<<<<< develop
-=======
->>>>>>> Vista de Canciones de Playlist
-=======
->>>>>>> playlist
+
   getPlaylistSongs(external){
     return this.httpClient.get(`${this.AUTH_SERVER_ADDRESS}/playlist/songsList/`+ external);
   }
-
-
-<<<<<<< HEAD
-<<<<<<< develop
-=======
->>>>>>> Servicio Plaulist
-=======
->>>>>>> Vista de Canciones de Playlist
-=======
->>>>>>> playlist
 }
